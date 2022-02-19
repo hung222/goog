6b5dfc9bf4c7d2ddfaebcb7b346d527f75bffc0f
hung222 <90371118+hung222@users.noreply.github.com>
2022-02-19 10:19:57 +0700

    Update AppServiceProvider.php

--- ankiop/app/Providers/AppServiceProvider.php
+++ ankiop/app/Providers/AppServiceProvider.php
@@ -12,9 +12,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {
-        //
+   {
+    if (env('APP_ENV') == 'Production') {
+        URL::forceScheme('https');
     }
+}
 
     /**
      * Bootstrap any application services.
