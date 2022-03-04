c6819dc99f021bc8c95eba4dc8c3abe566ffba37
hung222 <90371118+hung222@users.noreply.github.com>
2022-03-04 09:28:50 +0700

    Update ht.blade.php

--- ankiop/resources/views/ht.blade.php
+++ ankiop/resources/views/ht.blade.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> @yield('title')</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="container">
+        @section('content')
+            <p>chiều nay sẽ rửa máy lọc kia</p>
+        @show
+    </div>
+</body>
+</html>
 
-{{!!fffffffffffffffffffffffffffffffff!!}}
