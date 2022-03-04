791db5ce4f93b48e7a45156101d61fcdbb74a5aa
hung222 <90371118+hung222@users.noreply.github.com>
2022-03-04 10:46:40 +0700

    Update ht.blade.php

--- ankiop/resources/views/ht.blade.php
+++ ankiop/resources/views/ht.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> @yield('title')</title>
+    <title> @yield('giachoi')</title>
     <link rel="stylesheet" href="">
 </head>
 <body>
     <div class="container">
         @section('content')
             @for($i = giachoi; $i < 10; $i++)
-    {{ $giachoi }} <br/>
+  in ra giá trị  {{ $giachoi }} <br/>
 @endfor
         @show
     </div>
