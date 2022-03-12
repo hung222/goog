89312620f8d393b94e4a981fa723ca47ccf227c7
hung222 <90371118+hung222@users.noreply.github.com>
2022-03-12 09:05:30 +0700

    Add files via upload

---
+++ ankiop/database/migrations/2022_03_10_115832_create_gb_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGbTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gb', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps('mobilegb');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gb');
+    }
+}
