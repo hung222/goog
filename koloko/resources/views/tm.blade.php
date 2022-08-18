9142e063f0fec5645ffdfe802b0adfa10af6a626
jiyop <jiji333@gmail.com>
2022-08-18 16:59:04 +0700

    upload thu muc koloko vo heroku

---
+++ koloko/resources/views/tm.blade.php
@@ -0,0 +1,9 @@
+@extends('tgh')
+
+@section('title','đây là trang chủ')
+
+@section('content')
+
+<p> Đây là phần content</p>
+
+@endsection
---
+++ koloko/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/tgh', function ()    {
+     return view('tgh', ['name' => 'test']);
+});
+Route::get('call-view', function () {
+   return view('home');
+});
