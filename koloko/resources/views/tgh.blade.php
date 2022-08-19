28f95a3e36b825ff80121ef7ab289f4e52421a0e
hung222 <90371118+hung222@users.noreply.github.com>
2022-08-19 11:01:38 +0700

    Add files via upload

---
+++ koloko/resources/views/tgh.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> @yield('title')alibabababa</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+<h1>hello</h1>
+<h2>tghtghtghtghtgh</h2>
+<body bgcolor="red">
+    <div class="container">
+        @section('content')
+        @show
+    </div>
+</body>
+</html>
